7955c3b04b9b536f3854b2fa4ee79c512a4abdf2
shauntc <shaun@shauntc.com>
2016-09-12 12:06:00 -0700

    Initial Commit

---
+++ MathGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MathGame
+//
+//  Created by Shaun Campbell on 2016-09-12.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MathGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathGame
+//
+//  Created by Shaun Campbell on 2016-09-12.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
